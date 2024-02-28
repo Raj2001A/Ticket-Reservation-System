a788453185b604170ce2b119c74e62efaa66b802
Raja Rajeswaran A <112389678+Raj2001A@users.noreply.github.com>
2024-02-28 08:17:09 +0530

    Add files via upload

---
+++ Ticket Reservation System/app/src/main/java/com/example/Raja/myapplication/Required/ThankYou.java
@@ -0,0 +1,18 @@
+package com.example.Raja.myapplication.Required;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.Raja.myapplication.R;
+
+public class ThankYou extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thankyou);
+
+    }
+
+}
